91ab593dc6561df38beea4547a731302fca054c9
SEJUN LEE <leesjun29@gmail.com>
2023-06-11 23:44:54 +0900

    Add files via upload

--- hw13/_build/default/hw13.ml
+++ hw13/_build/default/hw13.ml
@@ -2,7 +2,8 @@ let rec interp_expr (ast : Ast.expr) ( (e , m): (Env.t *  Memory.t)) : Value.t =
   match ast with
   | Ast.Num n -> Value.NumV n
   | Ast.Ref r ->  Value.AddrV(Env.find r e)
-  | Ast.Id x -> let addr = Env.find x e in Memory.find addr m
+  | Ast.Id x -> let addr = Env.find x e 
+                in Memory.find addr m
   | Ast.Bool b -> Value.BoolV b
   | Ast.Add (e1, e2) ->
     let v1 = interp_expr e1 (e,m) in
@@ -69,8 +70,7 @@ match ast with
       | _ -> failwith (Format.asprintf "[Error] Not an address: %a" Ast.pp_expr e1)
     in
     let v = interp_expr e2 (e, m) in
-    let m' = Memory.add addr v m in
-    (e, m')
+    (e, Memory.add addr v m)
 
 
   (* x = *e *)
@@ -83,67 +83,52 @@ match ast with
       )
     in 
     let addr' = Memory.find addr m in
-    let m' = Memory.add x addr' m in (e,m')
+    (e, Memory.add x addr' m)
 
   (* e? s1^- : s2^- *)
-  (* | Ast.IfStmt  (e1,slst1, slst2) ->  
-    let v = interp_expr e1 (e, m) in
-      let (e', m') =
-      match v with
-      | Value.BoolV true -> interp_stmt_list slst1 (e, m)
-      | Value.BoolV false -> interp_stmt_list slst2 (e, m)
-      | _ -> failwith (Format.asprintf "[Error] Not a boolean: %a" Ast.pp_expr e1)
-    in (e', m') *)
-  | Ast.IfStmt  (e1,slst1, slst2) ->  
-    let v = interp_expr e1 (e, m) in
-    let rec interp_stmt_list (slst : Ast.stmt list) ((e, m) : (Env.t * Memory.t)) : (Env.t * Memory.t) =
-      match slst with
-      | [] -> (e, m)
-      | stmt :: rest ->
-        let (e', m') = interp_stmt stmt (e, m) in
-        interp_stmt_list rest (e', m') in
-     let (e', m') =
-         match v with
-        | Value.BoolV true -> interp_stmt_list slst1 (e, m)
-        | Value.BoolV false -> interp_stmt_list slst2 (e, m)
-        | _ -> failwith (Format.asprintf "[Error] Not a boolean: %a" Ast.pp_expr e1)
-     in (e', m') 
-    
-
-  (* while e slst *)
-  (* | Ast.LoopStmt (e1, slst) ->
-    let rec loop (e, m) =
-      let v = interp_expr e1 (e, m) in
-      match v with
-      | Value.BoolV true ->
-        let (e', m') = interp_stmt_list slst (e, m) in
-        loop (e', m')
-      | Value.BoolV false -> (e, m)
-      | _ -> failwith (Format.asprintf "[Error] Not a boolean: %a" Ast.pp_expr e1)
-      in loop (e, m)  *)
-  
-  | Ast.LoopStmt (e1, slst) ->
-    let rec loop (e, m) =
-      let v = interp_expr e1 (e, m) in
+     | Ast.IfStmt  (e1,slst1, slst2) ->  
       let rec interp_stmt_list (slst : Ast.stmt list) ((e, m) : (Env.t * Memory.t)) : (Env.t * Memory.t) =
-        match slst with
+        (match slst with
         | [] -> (e, m)
         | stmt :: rest ->
           let (e', m') = interp_stmt stmt (e, m) in
-          interp_stmt_list rest (e', m') in
-      match v with
-      | Value.BoolV true ->
-        let (e', m') = interp_stmt_list slst (e, m) in
-        loop (e', m')
-      | Value.BoolV false -> (e, m)
-      | _ -> failwith (Format.asprintf "[Error] Not a boolean: %a" Ast.pp_expr e1)
-      in loop (e, m) 
-  (* and interp_stmt_list (slst : Ast.stmt list) ((e, m) : (Env.t * Memory.t)) : (Env.t * Memory.t) =
-  match slst with
-  | [] -> (e, m)
-  | stmt :: rest ->
-    let (e', m') = interp_stmt stmt (e, m) in
-    interp_stmt_list rest (e', m') *)
+          interp_stmt_list rest (e', m') )
+          (* stmt_list 정의 끝 *)
+        in
+         let (_,m') = ( match interp_expr e1 (e, m) with
+            | Value.BoolV true -> interp_stmt_list slst1 (e, m)
+            | Value.BoolV false -> interp_stmt_list slst2 (e, m) 
+            | _ -> failwith (Format.asprintf "[Error] Not a boolean: %a" Ast.pp_expr e1)
+         )
+      in (e, m')
+
+  (* while e slst *)
+  | Ast.LoopStmt (e1, slst) ->
+    let rec interp_stmt_list (slst : Ast.stmt list) ((e, m) : (Env.t * Memory.t)) : (Env.t * Memory.t) =
+      (match slst with
+      | [] -> (e, m)
+      | stmt :: rest ->
+        let (e', m') = interp_stmt stmt (e, m) in
+        interp_stmt_list rest (e', m')) in
+        (* stmt_list 정의 끝 *)
+    let rec loop (e, m) =
+        match interp_expr e1 (e, m) with
+            | Value.BoolV true ->
+              let (_, m') = interp_stmt_list slst (e, m) in
+              loop (e, m')
+            | Value.BoolV false -> (e, m)
+            | _ -> failwith (Format.asprintf "[Error] Not a boolean: %a" Ast.pp_expr e1)
+        in loop (e, m)    
+      
+    (* let rec loop (e, m) =
+        match interp_expr e1 (e, m) with
+            | Value.BoolV true -> interp_stmt_list slst (e, m)
+            | Value.BoolV false -> (e, m)
+            | _ -> failwith (Format.asprintf "[Error] Not a boolean: %a" Ast.pp_expr e1)
+        in loop (e, m)  *)
+      
+
+    
 
 let interp_prog (ast : Ast.prog)  : (Env.t *  Memory.t) = 
 let rec interp_stmt_list (slst : Ast.stmt list) ((e, m) : (Env.t * Memory.t)) : (Env.t * Memory.t) =
@@ -172,5 +157,9 @@ let _ = AddrManager.init ()
 let prog = "def pc; def c; def z; c = 5; pc = &c;"
 let _ = assert (interp_prog (ParserMain.parse prog) = ([("z", 2); ("c", 1); ("pc", 0)], [(0, Value.AddrV 1); (1,Value.NumV 5)])) 
 let _ = AddrManager.init ()
- (* -> ([("z", 2); ("c", 1); ("pc", 0)], [(0, Value.AddrV 1); (1,Value.NumV 5)]) *)
-  *)
+(* -> ([("z", 2); ("c", 1); ("pc", 0)], [(0, Value.AddrV 1); (1,Value.NumV 5)]) *)
+let prog = "def x; def y; x = 3; y = 0; while x > 0 { y = y + x; x = x - 1;}"
+let _ = assert(interp_prog (ParserMain.parse prog) = ([("y", 1); ("x", 0)], [(0, Value.NumV 0); (1, Value.NumV 6)]))
+let _ = AddrManager.init ()
+(* -> ([("y", 1); ("x", 0)], [(0, Value.NumV 0); (1, Value.NumV 6)]) *)
+ *)
